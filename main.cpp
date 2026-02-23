2a870f3014b84a08c38ce4c08cd72384843d4704
Makai Marcell <hello@makaimarcell.com>
2026-02-23 12:29:34 +0100

    hibák javítása

--- main.cpp
+++ main.cpp
@@ -4,26 +4,27 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // HIBA: NELEMENTS nincs definiálva
+    int *b = new int[N_ELEMENTS];
 
-    std::cout << '1-100 ertekek duplazasa' // HIBA: '...' char literál, szöveghez "..." kell + hiányzik a pontosvessző a sor végéről
-    for (int i = 0;) // HIBA: a for ciklus szintaxisa hibás (hiányzik a feltétel és a léptetés)
+    std::cout << "1-100 ertekek duplazasa";
+    
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
 
-    for (int i = 0; i; i++) // HIBA: i kezdetben 0, a feltétel (i) hamis, ezért a ciklus sosem fut le (valószínű i < N_ELEMENTS kell)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" // HIBA: hiányzik a pontosvessző a sor végéről
+        std::cout << "Ertek:";
     }    
 
     std::cout << "Atlag szamitasa: " << std::endl;
 
-    int atlag; // HIBA: nincs inicializálva (szemét értékről indul), ezért rossz átlag jön ki
+    int atlag = 0;
 
-    for (int i = 0; i < N_ELEMENTS, i++) // HIBA: vessző van pontosvessző helyett
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] // HIBA: hiányzik a pontosvessző a sor végéről
+        atlag += b[i];
     }
 
     atlag /= N_ELEMENTS;
